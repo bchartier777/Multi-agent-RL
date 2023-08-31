73111cd47644338101c89f78c10803c6aaa33d47
Bernard Chartier <135620107+bchartier777@users.noreply.github.com>
2023-08-31 07:30:47 -0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,130 @@
+import torch
+import random
+import numpy as np
+import argparse
+import logging, os, copy
+
+
+class ReplayBuffer(object):
+    def __init__(self, args):
+        self.N = args.N  # The number of agents
+        self.buffer_size = args.buffer_size
+        self.batch_size = args.batch_size
+        self.count = 0
+        self.current_size = 0
+        self.buffer_obs_n, self.buffer_a_n, self.buffer_r_n, self.buffer_s_next_n, self.buffer_done_n = [], [], [], [], []
+        for agent_id in range(self.N):
+            self.buffer_obs_n.append(np.empty((self.buffer_size, args.obs_dim_n[agent_id])))
+            self.buffer_a_n.append(np.empty((self.buffer_size, args.action_dim_n[agent_id])))
+            self.buffer_r_n.append(np.empty((self.buffer_size, 1)))
+            self.buffer_s_next_n.append(np.empty((self.buffer_size, args.obs_dim_n[agent_id])))
+            self.buffer_done_n.append(np.empty((self.buffer_size, 1)))
+
+    def store_transition(self, obs_n, a_n, r_n, obs_next_n, done_n):
+        for agent_id in range(self.N):
+            self.buffer_obs_n[agent_id][self.count] = obs_n[agent_id]
+            self.buffer_a_n[agent_id][self.count] = a_n[agent_id]
+            self.buffer_r_n[agent_id][self.count] = r_n[agent_id]
+            self.buffer_s_next_n[agent_id][self.count] = obs_next_n[agent_id]
+            self.buffer_done_n[agent_id][self.count] = done_n[agent_id]
+        self.count = (self.count + 1) % self.buffer_size  # When the 'count' reaches max_size, it will be reset to 0.
+        self.current_size = min(self.current_size + 1, self.buffer_size)
+
+    def sample(self, ):
+        index = np.random.choice(self.current_size, size=self.batch_size, replace=False)
+        batch_obs_n, batch_a_n, batch_r_n, batch_obs_next_n, batch_done_n = [], [], [], [], []
+        for agent_id in range(self.N):
+            batch_obs_n.append(torch.tensor(self.buffer_obs_n[agent_id][index], dtype=torch.float))
+            batch_a_n.append(torch.tensor(self.buffer_a_n[agent_id][index], dtype=torch.float))
+            batch_r_n.append(torch.tensor(self.buffer_r_n[agent_id][index], dtype=torch.float))
+            batch_obs_next_n.append(torch.tensor(self.buffer_s_next_n[agent_id][index], dtype=torch.float))
+            batch_done_n.append(torch.tensor(self.buffer_done_n[agent_id][index], dtype=torch.float))
+
+        return batch_obs_n, batch_a_n, batch_r_n, batch_obs_next_n, batch_done_n
+
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+    """From https://github.com/udacity/deep-reinforcement-learning/blob/master/ddpg-pendulum/ddpg_agent.py"""
+
+    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        self.state = x + dx
+        return self.state
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--max_train_steps", type=int, default=int(1e6), help=" Maximum number of training steps")
+    parser.add_argument("--env_name", type=str, default="simple_speaker_listener", help="simple_speaker_listener or simple_spread")
+    parser.add_argument("--env_lib", type=str, default="mpe", help="petting_zoo or mpe or multi_mujoco")
+    parser.add_argument("--episode_limit", type=int, default=25, help="Maximum number of steps per episode")
+    parser.add_argument("--evaluate_freq", type=float, default=1000, help="Evaluate the policy every 'evaluate_freq' steps")
+    parser.add_argument("--evaluate_times", type=float, default=3, help="Evaluate times")
+    parser.add_argument("--max_action", type=float, default=1.0, help="Max action")
+    parser.add_argument("--gumb_softmax_action", type=bool, default=False, help="Apply Gumbel Softmax to actions, Actor, Critic_MADDPG only")
+    parser.add_argument("--random_steps", type=float, default=0, help="Number of random actions")
+                                        # 5e4 for full run
+    parser.add_argument("--algorithm", type=str, default="MADDPG", help="MADDPG or MATD3")
+    parser.add_argument("--buffer_size", type=int, default=int(1e6), help="The capacity of the replay buffer")
+    parser.add_argument("--batch_size", type=int, default=1024, help="Batch size") # Was 1024
+    parser.add_argument("--seed", type=int, default=0, help="Seed for models")
+    parser.add_argument("--hidden_dim", type=int, default=64, help="The number of neurons in hidden layers of the neural network")
+    parser.add_argument("--noise", type=str, default="gaussian", help="gaussian or ounoise")
+    parser.add_argument("--noise_std_min", type=float, default=0.05, help="The std of Gaussian noise for exploration")
+    parser.add_argument("--noise_std_init", type=float, default=0.2, help="The std of Gaussian noise for exploration")
+    parser.add_argument("--noise_decay_steps", type=float, default=3e5, help="How many steps before the noise_std decays to the minimum")
+    parser.add_argument("--use_noise_decay", type=bool, default=True, help="Whether to decay the noise_std")
+    parser.add_argument("--lr_a", type=float, default=5e-4, help="Learning rate of actor")
+    parser.add_argument("--lr_c", type=float, default=5e-4, help="Learning rate of critic")
+    parser.add_argument("--gamma", type=float, default=0.95, help="Discount factor")
+    parser.add_argument("--tau", type=float, default=0.01, help="Softly update the target network")
+    parser.add_argument("--use_orthogonal_init", type=bool, default=True, help="Orthogonal initialization")
+    parser.add_argument("--use_grad_clip", type=bool, default=True, help="Gradient clip")
+
+    # --------------------------------------MATD3--------------------------------------------------------------------
+    parser.add_argument("--policy_noise", type=float, default=0.2, help="Target policy smoothing")
+    parser.add_argument("--noise_clip", type=float, default=0.5, help="Clip noise")
+    parser.add_argument("--policy_update_freq", type=int, default=2, help="The frequency of policy updates")
+
+    args = parser.parse_args()
+    args.noise_std_decay = (args.noise_std_init - args.noise_std_min) / args.noise_decay_steps
+
+    return args
+
+def setup_logger(filename):
+    """ set up logger with filename. """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    handler = logging.FileHandler(filename, mode='w')
+    handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s--%(levelname)s--%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
+
+def create_res_dir(args):    # create folder to save result
+    env_dir = os.path.join('./results', args.env_name)
+    if not os.path.exists(env_dir):
+        os.makedirs(env_dir)
+    total_files = len([file for file in os.listdir(env_dir)])
+    result_dir = os.path.join(env_dir, f'{total_files + 1}')
+    os.makedirs(result_dir)
+    return result_dir
